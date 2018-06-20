38b572c19ad2edcfdc5342eefda96a9525e57eaf
lijiabin01 <lijiabin01@baidu.com>
2018-06-20 16:46:30 +0800

    PC端页脚组件

--- Demo_2/11-01/src/js/components/pc_index.js
+++ Demo_2/11-01/src/js/components/pc_index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import PCHeader from './pc_header'
+import PCFooter from './pc_footer'
 
 export default class Root extends React.Component{
   render(){
     return(
       <div>
         <PCHeader></PCHeader>
+        <PCFooter></PCFooter>
       </div>
     );
   };
