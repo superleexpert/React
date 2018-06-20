ce42766831d0dd6a1eb20dd74aa86091a8359b29
lijiabin01 <lijiabin01@baidu.com>
2018-06-20 19:41:36 +0800

    登录、注册表单组件

--- Demo_2/11-01/src/js/components/pc_header.js
+++ Demo_2/11-01/src/js/components/pc_header.js
@@ -1,19 +1,71 @@
 import React from 'react';
 import {Row, Col} from 'antd'
-import {Menu, Icon} from 'antd'
+import {
+	Menu,
+	Icon,
+	Tabs,
+	message,
+	Form,
+	Input,
+	Button,
+	CheckBox,
+	Modal
+} from 'antd';
 const MenuItemGroup = Menu.ItemGroup;
 const SubMenu = Menu.SubMenu;
+const FormItem = Form.Item
+const TabPane = Tabs.TabPane;
 
-export default class Root extends React.Component{
+class PCHeader extends React.Component{
 
   constructor(){
     super();
-    this.state={
-      current: 'top'
+		this.state = {
+			current: 'top',
+			modalVisible: false,
+			action: 'login',
+			hasLogined: false,
+			userNickName: '',
+			userid: 0
     };
-  }
+  };
+
+  setModalVisible(value)
+	{
+		this.setState({modalVisible: value});
+  };
+  
+  handleClick(e) {
+		if (e.key == "register") {
+			this.setState({current: 'register'});
+			this.setModalVisible(true);
+		} else {
+			{
+				this.setState({current: e.key});
+			}
+		}
+  };
+  
+  handleSubmit(e)
+	{
+  };
 
   render(){
+    let {getFieldProps} = this.props.form;
+		const userShow = this.state.hasLogined
+			? <Menu.Item key="logout" class="register">
+					<Button type="primary" htmlType="button">{this.state.userNickName}</Button>
+					&nbsp;&nbsp;
+					<Link target="_blank" to={`/usercenter`}>
+						<Button type="dashed" htmlType="button">个人中心</Button>
+					</Link>
+					&nbsp;&nbsp;
+					<Button type="ghost" htmlType="button" onClick={this.logout.bind(this)}>退出</Button>
+				</Menu.Item>
+			: <Menu.Item key="register" class="register">
+				<Icon type="appstore"/>注册/登录
+      </Menu.Item>;
+      
     return(
       <header>
         <Row>
@@ -25,7 +77,7 @@ export default class Root extends React.Component{
             </a>
           </Col>
           <Col span={16}>
-            <Menu mode="horizontal" selectedKeys={[this.state.current]}>
+          <Menu mode="horizontal" onClick={this.handleClick.bind(this)} selectedKeys={[this.state.current]}>
               <Menu.Item key="top">
                 <Icon type="appstore"/>头条
               </Menu.Item>
@@ -50,7 +102,28 @@ export default class Root extends React.Component{
               <Menu.Item key="shishang">
                 <Icon type="appstore"/>时尚
               </Menu.Item>
+              {userShow}
             </Menu>
+
+						<Modal title="用户中心" wrapClassName="vertical-center-modal" visible={this.state.modalVisible} onCancel= {()=>this.setModalVisible(false)} onOk={() => this.setModalVisible(false)} okText = "关闭">
+							<Tabs type="card">
+								<TabPane tab="注册" key="2">
+									<Form horizontal onSubmit={this.handleSubmit.bind(this)}>
+										<FormItem label="账户">
+											<Input placeholder="请输入您的账号" {...getFieldProps('r_userName')}/>
+										</FormItem>
+										<FormItem label="密码">
+											<Input type="password" placeholder="请输入您的密码" {...getFieldProps('r_password')}/>
+										</FormItem>
+										<FormItem label="确认密码">
+											<Input type="password" placeholder="请再次输入您的密码" {...getFieldProps('r_confirmPassword')}/>
+										</FormItem>
+										<Button type="primary" htmlType="submit" >注册</Button>
+									</Form>
+								</TabPane>
+							</Tabs>
+						</Modal>
+
           </Col>
           <Col span={2}></Col>
         </Row>
@@ -58,3 +131,5 @@ export default class Root extends React.Component{
     );
   };
 }
+
+export default PCHeader = Form.create({})(PCHeader);
