f3acafe3d6d8358870979900a3c628ba8a72ccf8
lijiabin01 <lijiabin01@baidu.com>
2018-06-22 14:56:29 +0800

    common comments

--- Demo_2/11-01/src/js/components/mobile_news_details.js
+++ Demo_2/11-01/src/js/components/mobile_news_details.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Row, Col, BackTop} from 'antd';
 import MobileHeader from './mobile_header';
 import MobileFooter from './mobile_footer';
+import CommonComments from './common_comments';
 export default class MobileNewsDetails extends React.Component {
 	constructor() {
 		super();
@@ -29,6 +30,8 @@ export default class MobileNewsDetails extends React.Component {
 					<Row>
 						<Col span={24} className="container">
 							<div class="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
+							<hr/>
+							<CommonComments uniquekey={this.props.params.uniquekey}/>
 						</Col>
 					</Row>
 					<MobileFooter></MobileFooter>
--- Demo_2/11-01/src/js/components/pc_news_details.js
+++ Demo_2/11-01/src/js/components/pc_news_details.js
@@ -3,6 +3,7 @@ import {Row, Col, BackTop} from 'antd';
 import PCHeader from './pc_header';
 import PCFooter from './pc_footer';
 import PCNewsImageBlock from './pc_news_image_block';
+import CommonComments from './common_comments';
 export default class PCNewsDetails extends React.Component {
 	constructor() {
 		super();
@@ -17,7 +18,7 @@ export default class PCNewsDetails extends React.Component {
 		fetch("http://newsapi.gugujiankong.com/Handler.ashx?action=getnewsitem&uniquekey=" + this.props.params.uniquekey, myFetchOptions).then(response => response.json()).then(json => {
 			this.setState({newsItem: json});
 			document.title = this.state.newsItem.title + " - React News | React 驱动的新闻平台";
-		})
+		});
 	};
 	createMarkup() {
 		return {__html: this.state.newsItem.pagecontent};
@@ -30,6 +31,8 @@ export default class PCNewsDetails extends React.Component {
 					<Col span={2}></Col>
 					<Col span={14} className="container">
 						<div class="articleContainer" dangerouslySetInnerHTML={this.createMarkup()}></div>
+						<hr/>
+						<CommonComments uniquekey={this.props.params.uniquekey}/>
 					</Col>
 					<Col span={6}>
 						<PCNewsImageBlock count={40} type="top" width="100%" cardTitle="相关新闻" imageWidth="150px"/>
